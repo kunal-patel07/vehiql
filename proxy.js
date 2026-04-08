23b9cde6d60cfcfb810cca136c7d5d2139eebd28
kunal-patel07 <kunnalpatel2004@gmail.com>
2026-04-08 22:16:19 +0530

    refactor: migrate middleware logic to proxy.js and initialize new Arcjet token bucket configuration

--- proxy.js
+++ proxy.js
@@ -1,4 +1,5 @@
-import arcjet, { createMiddleware, detectBot, shield } from '@arcjet/next';
+  
+   import arcjet, { createMiddleware, detectBot, shield } from '@arcjet/next';
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 import { NextResponse } from 'next/server';
 
